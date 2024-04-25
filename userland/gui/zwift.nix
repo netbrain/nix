9ea1e3ad9cb1c20c23ebbbbe344085d969d6e796
Kim Eik <kim@heldig.org>
2024-04-25 09:31:42 +0200

    added updated zwift.sh

--- userland/gui/zwift.nix
+++ userland/gui/zwift.nix
@@ -5,7 +5,7 @@
       "zwift.sh" = {
         source = pkgs.fetchurl {
             url = "https://raw.githubusercontent.com/netbrain/zwift/master/zwift.sh";
-            hash = "sha256-RQJ6gPHXiY8CNeGGgQDnwG3mHzmMo3Yl6Ninnx+mJNY=";
+            hash = "sha256-RA94NjRJU3EIK25ZMbCYriQeb9wsidTR8YvEBIWl0UY=";
         };
         target = ".local/bin/zwift";
         executable = true;
